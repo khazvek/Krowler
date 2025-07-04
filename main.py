0aad6b5add031a162173e3253cc047880ab7a4ba
k. <105748534+khazvek@users.noreply.github.com>
2025-07-04 23:04:38 +0200

    Add GitHub user scanner module

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import sys
 from colorama import init, Fore
 from pyfiglet import figlet_format
 
-from modules import whois_lookup, dns_lookup, ip_tracker, port_scanner
+from modules import (
+    whois_lookup,
+    dns_lookup,
+    ip_tracker,
+    port_scanner,
+    github_user_scanner,
+)
 
 init(autoreset=True)
 
@@ -16,6 +22,7 @@ def menu():
     print(Fore.GREEN + "2. DNS Lookup")
     print(Fore.GREEN + "3. IP Tracker")
     print(Fore.GREEN + "4. Port Scanner")
+    print(Fore.GREEN + "5. GitHub User Scanner")
     print(Fore.RED + "0. Exit")
 
 
@@ -32,6 +39,8 @@ def main():
             ip_tracker.run()
         elif choice == '4':
             port_scanner.run()
+        elif choice == '5':
+            github_user_scanner.run()
         elif choice == '0':
             print(Fore.CYAN + "Goodbye!")
             sys.exit()
